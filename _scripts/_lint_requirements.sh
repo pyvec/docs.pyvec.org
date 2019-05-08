34f300c0640b8af83ea6fe7b4f81be4e087d78d3
Honza Javorek <mail@honzajavorek.cz>
2019-05-08 14:08:14 +0200

    change how linting works

--- _scripts/_lint_requirements.sh
+++ _scripts/_lint_requirements.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-# Lint Python code
-pipenv run pylama
-
 # Make sure requirements.txt are in sync with Pipfile.lock
-pipenv run pipenv_to_requirements -f -o ci-requirements.txt
+pipenv lock --requirements | sed -e 's#pypi.org/simple$#pypi.org/simple/#' > ci-requirements.txt
 if ! diff requirements.txt ci-requirements.txt > /dev/null
 then
   rm ci-requirements.txt
@@ -13,12 +10,9 @@ then
   echo ""
   echo ""
   echo "The requirements.txt file is not up to date. Run" \
-    "'pipenv run requirements' and commit the changes." \
+    "'pipenv lock --requirements > requirements.txt' and commit changes." \
     "See ${help_url} for more info."
   exit 1
 else
   rm ci-requirements.txt
 fi
-
-# Check external links
-pipenv run sphinx-build -nW -b linkcheck . _build
