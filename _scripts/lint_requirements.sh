9e8633cb0eac3f9e3d023c29c8e48704c1445a72
Honza Javorek <mail@honzajavorek.cz>
2019-05-08 14:22:02 +0200

    update lockfiles

---
+++ _scripts/lint_requirements.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Make sure requirements.txt are in sync with Pipfile.lock
+
+set -e
+
+add_slash_re='s#pypi.org/simple/?$#pypi.org/simple/#'
+remove_comment_re='s# *;.*##'
+
+cat requirements.txt | sed -e "$add_slash_re" | sed -e "$remove_comment_re" > normalized-requirements.txt
+pipenv lock --requirements | sed -e "$add_slash_re" | sed -e "$remove_comment_re" > just-locked-requirements.txt
+
+if ! diff normalized-requirements.txt just-locked-requirements.txt
+then
+  rm *-requirements.txt
+  help_url='https://pyvec-guide.readthedocs.io/CONTRIBUTING.html#zavislosti'
+  echo ""
+  echo ""
+  echo "The requirements.txt file is not up to date. Run" \
+    "'pipenv lock --requirements > requirements.txt' and commit changes." \
+    "See ${help_url} for more info."
+  exit 1
+else
+  rm *-requirements.txt
+fi
